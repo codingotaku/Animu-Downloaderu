2fd1f2de4de2327533e800fe12b6aed2d47d7c92
CodingOtaku <otakucoding@gmail.com>
2018-12-23 14:38:58 +0530

    updated dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,19 +3,12 @@
  *
  */
 module Animu_Downloaderu {
-	exports com.codingotaku.apps.download;
-	exports com.codingotaku.apps.util;
-	exports com.codingotaku.apps;
-	exports com.codingotaku.apps.callback;
-	exports com.codingotaku.apps.custom;
-	exports com.codingotaku.apps.source;
-	opens com.codingotaku.apps to javafx.fxml;
 	requires java.desktop;
 	requires java.prefs;
-	requires javafx.base;
 	requires javafx.controls;
 	requires javafx.fxml;
-	requires transitive javafx.graphics;
 	requires javafx.web;
 	requires org.jsoup;
+	opens com.codingotaku.apps to javafx.fxml;
+	exports com.codingotaku.apps;
 }
